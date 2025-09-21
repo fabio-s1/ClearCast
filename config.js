0503bd55d55c9cefa2b5bd1c43e27bece4fadbd8
Fabio Siqueira <contact@fabios1.com>
2025-09-21 19:41:41 -0300

    Delete config.js
    Conflict with local files due to .gitignore.

--- config.js
+++
@@ -1,4 +0,0 @@
-// Store your OpenWeatherMap API key here.
-// IMPORTANT: Make sure this file is listed in your .gitignore to prevent it from being committed to GitHub.
-
-const API_KEY = "API_KEY";
