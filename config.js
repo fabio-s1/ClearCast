b5f3e0f28353a82a933dace3830a9cf98af9e73b
Fabio Siqueira <contact@fabios1.com>
2025-09-21 19:29:01 -0300

    chore: add configuration file for API key and update gitignore

---
+++ config.js
@@ -0,0 +1,4 @@
+// Store your OpenWeatherMap API key here.
+// IMPORTANT: Make sure this file is listed in your .gitignore to prevent it from being committed to GitHub.
+
+const API_KEY = "API_KEY";
